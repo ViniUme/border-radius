e902acce844a8589567200178adcee4154dcfb2f
ViniUme <viniciusumetsu210@gmail.com>
2022-01-15 10:37:18 -0300

    diminished code

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-function Carregar(){
-    var tl_radius = window.document.getElementById('input-left-top').value;
-    var bl_radius = window.document.getElementById('input-left-bottom').value;
-    var tr_radius = window.document.getElementById('input-right-top').value;
-    var br_radius = window.document.getElementById('input-right-bottom').value;
-    var sectionBorder = window.document.getElementById('section-border');
-    var radius_code = window.document.getElementById('input-right-bottom').innerHTML;
-    var tl_code = window.document.getElementById('code-tl_radius').innerHTML;
-    var tr_code = window.document.getElementById('code-tr_radius').innerHTML;
-    var bl_code = window.document.getElementById('code-bl_radius').innerHTML;
-    var br_code = window.document.getElementById('code-br_radius').innerHTML;
+var section_border = document.getElementById('section-border')
 
-    sectionBorder.style.borderTopLeftRadius = tl_radius + 'px';
-    sectionBorder.style.borderBottomLeftRadius = bl_radius + 'px';
-    sectionBorder.style.borderTopRightRadius = tr_radius + 'px';
-    sectionBorder.style.borderBottomRightRadius = br_radius + 'px';
+function Input(id_border){
+    let border = document.getElementById(id_border)
+    let value = border.value
+    let code = document.getElementById('code-' + id_border)
 
-    if(tl_radius != 0){
-        window.document.getElementById('code-tl_radius').innerHTML = 'border-top-left-radius: ' + tl_radius + 'px;';
+    if(id_border == 'input-top-left'){
+        section_border.style.borderTopLeftRadius = `${value}px`
+        code.innerHTML = `border-top-left-radius: ${value}px`
     }
-    if(bl_radius != 0){
-        window.document.getElementById('code-bl_radius').innerHTML = 'border-bottom-left-radius: ' + bl_radius + 'px;';
+    else if(id_border == 'input-bottom-left'){
+        section_border.style.borderBottomLeftRadius = `${value}px`
+        code.innerHTML = `border-bottom-left-radius: ${value}px`
     }
-    if(tr_radius != 0){
-        window.document.getElementById('code-tr_radius').innerHTML = 'border-top-right-radius: ' + tr_radius + 'px;';
+    else if(id_border == 'input-top-right'){
+        section_border.style.borderTopRighttRadius = `${value}px`
+        code.innerHTML = `border-top-right-radius: ${value}px`
     }
-    if(br_radius != 0){
-        window.document.getElementById('code-br_radius').innerHTML = 'border-bottom-right-radius: ' + br_radius + 'px;';
+    else{
+        section_border.style.borderBottomRighttRadius = `${value}px`
+        code.innerHTML = `border-bottom-right-radius: ${value}px`
     }
 }
